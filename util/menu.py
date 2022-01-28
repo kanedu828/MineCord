7706d9f1e2c1dd5d39552cb71785cf6845b96693
kanedu828 <kanedu2000@gmail.com>
2022-01-28 12:52:39 -0500

    adjust buttons

--- util/menu.py
+++ util/menu.py
@@ -1,44 +1,57 @@
 from discord.ext import menus
+from discord.ext.menus.views import ViewMenu
 import discord
 
 
-class PageMenu(discord.ui.View):
+class PageMenu(ViewMenu):
 
-    def __init__(self, pages):
-        super().__init__()
+    def __init__(self, title, color, pages):
+        super().__init__(delete_message_after=True)
+        self.message_embed = discord.Embed(title=title, color=color)
         self.pages = pages
         self.current_page = 0
 
-    @discord.ui.button(label='◀')
-    async def back_button(self, button, interaction):
+    async def send_initial_message(self, ctx, channel):
+        self.message_embed.description = self.pages[0]
+        self.message_embed.set_footer(text=f'page {self.current_page + 1}/{len(self.pages)}')
+        return await self.send_with_view(channel, embed=self.message_embed)
+
+    @menus.button('◀')
+    async def on_back(self, payload):
         if self.current_page > 0:
             self.current_page -= 1
-            message_embed = interaction.message.embeds[0]
-            message_embed.description = self.pages[self.current_page]
-            message_embed.set_footer(text=f'page {self.current_page + 1}/{len(self.pages)}')
-            msg = await interaction.edit_original_message(embed=message_embed)
+            self.message_embed.description = self.pages[self.current_page]
+            self.message_embed.set_footer(text=f'page {self.current_page + 1}/{len(self.pages)}')
+            await self.message.edit(embed=self.message_embed)
 
-    @discord.ui.button(label='▶')
-    async def next_button(self, button, interaction):
+    @menus.button('▶')
+    async def on_next(self, payload):
         if self.current_page < len(self.pages) - 1:
             self.current_page += 1
-            message_embed = interaction.message.embeds[0]
-            message_embed.description = self.pages[self.current_page]
-            message_embed.set_footer(text=f'page {self.current_page + 1}/{len(self.pages)}')
-            msg = await interaction.edit_original_message(embed=message_embed)
+            self.message_embed.description = self.pages[self.current_page]
+            self.message_embed.set_footer(text=f'page {self.current_page + 1}/{len(self.pages)}')
+            await self.message.edit(embed=self.message_embed)
 
 
-class ConfirmationMenu(discord.ui.View):
-    def __init__(self):
-        super().__init__()
+class ConfirmationMenu(ViewMenu):
+    def __init__(self, message_embed):
+        super().__init__(delete_message_after=True)
+        self.message_embed = message_embed
         self.result = None
 
-    @discord.ui.button(label='Confirm')
-    async def confirm_button(self, buttom, interaction):
+    async def send_initial_message(self, ctx, channel):
+        return await self.send_with_view(channel, embed=self.message_embed)
+
+    @menus.button('\N{WHITE HEAVY CHECK MARK}')
+    async def do_confirm(self, payload):
         self.result = True
         self.stop()
 
-    @discord.ui.button(label='Cancel')
-    async def deny_button(self, button, interaction):
+    @menus.button('\N{CROSS MARK}')
+    async def do_deny(self, payload):
         self.result = False
         self.stop()
+
+    async def prompt(self, ctx):
+        await self.start(ctx, wait=True)
+        return self.result
